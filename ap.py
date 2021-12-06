7c5efcb4dc8283b5b9e45c4507356c65182390ed
erfan mostafa <erfanmoc1997@gmail.com>
2021-12-07 00:27:53 +0330

    this is fibo

---
+++ ap.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+#erfanmostafa
+#released under GPLv3+ Licence
+
+def fibo(num):
+    if  num == 1:
+        return 1
+    if num ==2:
+        return 1
+    return fibo(num -1 ) + fibo(num-2)
+    
+print(fibo(5))
